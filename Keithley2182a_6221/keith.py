d18370d468c0bc91e6254e908a4e1d9b28c4159e
safrie <safrie@sas.upenn.edu>
2020-08-07 17:29:23 -0400

    Updated set_filter_window w validation structure

--- Keithley2182a_6221/keith.py
+++ Keithley2182a_6221/keith.py
@@ -653,11 +653,12 @@ class Keith(Instrument):
         """Set filter_window of meas type to window and update Keithleys."""
         meas = self.meas_type(meas_idx)
         enable = meas.filter_on
-        if not lims.filt_window[0] <= window <= lims.filt_window[1]:
-            window = lims.filt_window_def
-            print(f"Filter window out of bounds ([{lims.filt_window[0]}, "
-                  + f"{lims.filt_window[1]}]).  Setting to default value "
-                  + f"({lims.filt_window_def}).")
+        inf = info.fwindow
+        if not inf['lim'][0] <= window <= inf['lim'][1]:
+            window = inf['def']
+            print(f"Filter window out of bounds ([{inf['lim'][0]}, "
+                  + f"{inf['lim'][1]}]).  Setting to default value "
+                  + f"({window}).")
         meas.set_filter_window(window)
         self.visa.set_filter_window(window)
         self.visa.set_filter(enable)
