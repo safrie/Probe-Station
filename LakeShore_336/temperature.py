e0f37f2ee73d38e75fa0d2604d1861dff030ed4d
safrie <safrie@sas.upenn.edu>
2020-08-11 17:25:37 -0400

    Removed validator from Temp.set_address
    - Validator is already in Instrument.set_address, so redundancy unneeded

--- LakeShore_336/temperature.py
+++ LakeShore_336/temperature.py
@@ -83,12 +83,7 @@ class Temp(Instrument):
     def set_address(self, addr: int) -> None:
         """Set GPIB address of 336, then check it's connected."""
         # TODO: Test set_address
-        if addr not in lims.addr:
-            addr = lims.addr_default
-            print(f"Given address not in valid range [{lims.addr[0]}, "
-                  + f"{lims.addr[1]}].  GPIB address set to default "
-                  + f"({lims.addr_default}).")
-        super().set_address(addr)
+        addr = super().set_address(addr)
         self.visa.check_connected(addr)
         return addr
 
