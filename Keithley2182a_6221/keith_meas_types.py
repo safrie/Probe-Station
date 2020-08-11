ea0e8c10fe16397edebf4a4afdeaabd75f650209
safrie <safrie@sas.upenn.edu>
2020-08-10 23:39:38 -0400

    Updated PDeltaLog docstring and init w validator
    - Units for current and time corrected in docstring
    - __init__ now refers to the correct dictionary entries

--- Keithley2182a_6221/keith_meas_types.py
+++ Keithley2182a_6221/keith_meas_types.py
@@ -396,11 +396,11 @@ class PDeltaLog(KeithMeasure):
     in Keith.
 
     attributes_
-        curr1: Start current in microamps (float)
-        curr2: End current in microamps (float)
+        curr1: Start current in amps (float)
+        curr2: End current in amps (float)
         num_points: Number of points in a single sweep (int)
         num_sweeps: Number of sweeps to perform (int)
-        pulse_width: Length of the high current pulse in microseconds (float)
+        pulse_width: Length of the high current pulse in seconds (float)
         cycle_time: Quasiperiod of the pulse delta cycle (lo-hi-lo) in seconds
                     (float)
         low_meas: Whether to measure voltage on second lo in the cycle (bool)
@@ -418,15 +418,16 @@ class PDeltaLog(KeithMeasure):
     def __init__(self) -> None:
         """Instantiate pulse delta log sweep."""
         super().__init__(4)
-        self.curr1 = 1.00
-        self.curr2 = 10.00
-        self.num_points = 10
-        self.num_sweeps = 1
-        self.pulse_width = 120
-        self.meas_delay = 16
-        self.cycle_time = 5.0
-        self.filter_idx = 0
-        self.filter_type = 'Moving'
+        info = self.info
+        self.curr1 = info.curr1['def']
+        self.curr2 = info.curr2['def']
+        self.num_points = info.points['def']
+        self.num_sweeps = info.sweeps['def']
+        self.pulse_width = info.width['def']
+        self.meas_delay = info.delay['def']
+        self.cycle_time = info.rate['def']
+        self.filter_idx = info.filt['def']
+        self.filter_type = info.filt['txt'][self.filter_idx]
 
     def get_meas_type_str(self) -> None:
         """Return type of measurement followed by a new line."""
